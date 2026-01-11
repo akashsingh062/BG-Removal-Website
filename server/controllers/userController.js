e3dd8e59557217b208974e8e8863e4598509ce8a
akashsingh062 <akash622003singh@gmail.com>
2026-01-11 16:41:57 +0530

    bug fix

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -6,7 +6,7 @@ import userModel from "../models/userModel.js"
 const clerkWebhooks = async () => {
     try {
         // create a svix instance with clerk webhook secret
-        const whook = Webhook(process.env.CLERK_WEBHOOK_SECRET)
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
 
         await whook.verify(JSON.stringify(req.body), {
             "svix-id": req.headers["svix-id"],
@@ -15,38 +15,41 @@ const clerkWebhooks = async () => {
         })
 
         const { data, type } = req.body
+
         switch (type) {
             case "user.created": {
+
                 const userData = {
                     clerkId: data.id,
-                    email: data.email_addresses[0].email_addresses,
-                    firstName: data.firstName,
-                    lastName: data.lastName,
+                    email: data.email_addresses[0].email_address,
+                    firstName: data.first_name,
+                    lastName: data.last_name,
                     photo: data.image_url
                 }
                 await userModel.create(userData)
-                resizeBy.JSON({})
+                res.json({})
                 break;
             }
             case "user.updated": {
                 const userData = {
-                    email: data.email_addresses[0].email_addresses,
-                    firstName: data.firstName,
-                    lastName: data.lastName,
+                    email: data.email_addresses[0].email_address,
+                    firstName: data.first_name,
+                    lastName: data.last_name,
                     photo: data.image_url
                 }
-                await userModel.findOneAndUpdate({clerkId:data.id}, userData)
-                res.JSON({})
+                await userModel.findOneAndUpdate({ clerkId: data.id }, userData)
+                res.json({})
                 break;
             }
             case "user.deleted": {
-                await userModel.findOneAndDelete({clerkId:data.id})
-                res.JSON({})
+                await userModel.findOneAndDelete({ clerkId: data.id })
+                res.json({})
                 break;
             }
         }
     } catch (error) {
         console.log(error.message)
+        res.json({ success: false, message: error.message })
     }
 }
 
