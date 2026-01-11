4ff9a4ac78f202b3681803aa5ffb2e04f29d9af0
akashsingh062 <akash622003singh@gmail.com>
2026-01-11 17:29:02 +0530

    test fix

--- server/server.js
+++ server/server.js
@@ -1,24 +1,28 @@
 import express from "express";
-import 'dotenv/config'
-import cors from 'cors'
+import "dotenv/config";
+import cors from "cors";
 import connectDB from "./configs/mongodb.js";
 import userRouter from "./routes/userRoutes.js";
+import { clerkWebhooks } from "./controllers/userController.js";
 
-// app config
-const PORT = process.env.PORT || 4000
-const app = express()
+const PORT = process.env.PORT || 4000;
+const app = express();
 
-// middlewares
-app.use(express.json())
-app.use(cors())
+app.use(cors());
 
-// api routes
-app.get('/', (req, res) => {
-    res.send("working")
-})
-app.use('/api/user', userRouter)
+app.get("/", (req, res) => {
+    res.send("working");
+});
+
+app.post(
+    "/api/user/webhooks",
+    express.raw({ type: "application/json" }),
+    clerkWebhooks
+);
+app.use(express.json());
+app.use("/api/user", userRouter);
 
 app.listen(PORT, async () => {
-    await connectDB()
-    console.log("app is running on port : ", PORT)
-})
+    await connectDB();
+    console.log("app is running on port:", PORT);
+});
