22da8d2a14de0d83537de913978ce281d52f4a31
akashsingh062 <akash622003singh@gmail.com>
2026-01-12 18:05:02 +0530

    -

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,7 +4,6 @@ import authUser from "../middlewares/auth.js";
 
 const userRouter = express.Router()
 
-
 userRouter.post("/webhooks", clerkWebhooks);
 userRouter.get("/credits", authUser, userCredits);
 userRouter.post("/pay-razor", authUser, paymentRazorpay);
