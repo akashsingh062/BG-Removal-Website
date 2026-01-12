cc446be03076bead405ab9a55e376d0acd36a33f
akashsingh062 <akash622003singh@gmail.com>
2026-01-12 17:59:55 +0530

    bug fix 2

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import express from "express";
-import "dotenv/config";
-import cors from "cors";
+import 'dotenv/config'
+import cors from 'cors'
 import connectDB from "./configs/mongodb.js";
 import userRouter from "./routes/userRoutes.js";
 import imageRouter from "./routes/imageRoutes.js";
@@ -8,10 +8,11 @@ import imageRouter from "./routes/imageRoutes.js";
 // app config
 const PORT = process.env.PORT || 4000
 const app = express()
+await connectDB()
 
-
-app.use(cors());
-await connectDB();
+// middlewares
+app.use(express.json())
+app.use(cors())
 
 // api routes
 app.get('/', (req, res) => {
@@ -23,15 +24,3 @@ app.use('/api/image', imageRouter)
 app.listen(PORT, async () => {
     console.log("app is running on port : ", PORT)
 })
-
-app.post(
-    "/api/user/webhooks",
-    express.raw({ type: "application/json" }),
-    clerkWebhooks
-);
-app.use(express.json());
-app.use("/api/user", userRouter);
-
-app.listen(PORT, async () => {
-    console.log("app is running on port:", PORT);
-});
